48a1874e29e6932a58975e362d577b0d98e07b4d
Uxue Laptop <thechuchu049@gmail.com>
2024-09-11 11:48:17 -0600

    feat: añade maquetacion y validacion de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,18 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatButtonModule} from "@angular/material/button";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
+  form = new FormGroup<formType>({
+    emailUser: new FormControl<string>('', {
+      nonNullable: true,
+      validators:[
+        Validators.required,
+        Validators.email
+      ]
+    }),
+    passwordUser: new FormControl<string>('', {
+      nonNullable: true,
+      validators:[
+        Validators.required,
+      ]
+    })
+  })
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  get ctrlEmailUser(){
+    return this.form.controls.emailUser;
+  }
+
+  get ctrlPasswordUser(){
+    return this.form.controls.passwordUser;
+  }
+
+}
+type formType={
+  emailUser: FormControl<string>,
+  passwordUser: FormControl<string>
 }
